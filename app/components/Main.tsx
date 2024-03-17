c4881595c54dc69007830772504fe17830c7be80
fourcolors <sterlingcobb@gmail.com>
2024-03-17 08:52:15 +0000

    move image to fix build

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import React, { useState } from "react";
 import NoggleSelector from "./NoggleSelect";
 import WebCamComponent from "./WebCam";
@@ -9,6 +10,7 @@ export default function Main() {
 
   return (
     <>
+      <Image src="/logo.png" alt="logo" width={400} height={200} />
       <NoggleSelector
         selectedNoggle={selectedNoggle}
         setSelectedNoggle={setSelectedNoggle}
